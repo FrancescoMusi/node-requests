22b763658400349f0c08baff56b64fec55875e60
Francesco Musi <francesco.musi03@gmail.com>
2018-12-17 23:28:28 +0100

    work

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+// saluta daniele
+app.get("/daniele", (req, res) => {
+  res.status(200).send("Ciao Daniele come va?");
+});
+
+// saluta l'argomento
+app.get("/saluta/:nome", (req, res) => {
+  res.status(200).send("ciao ${req.params.nome} come va");
+});
+
+// da nome ed età
+app.post("/info", (req, res) => {
+  res.status(200).json({
+	nome: "Francesco",
+ 	età: 15
+ });
+});
+
+// porta in ascolto
+app.listen(3000, () => {
+  console.log("App listening on port 3000...");
+});
