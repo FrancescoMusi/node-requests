8d8d8bfd4e1f51b4dfbc08d31260952d874b0c47
Francesco Musi <francesco.musi03@gmail.com>
2018-12-18 18:55:35 +0100

    work

--- esercizio 2.js
+++ esercizio 2.js
@@ -8,9 +8,6 @@ const app = express();
 //2° argomento = metodo
 //3° argomento = body (non serve in questo caso)
 
-fetch("https://api.paninidellospalla.it/info", {
-	method: "GET"
-})
 
 /* 
 ".then" serve per far eseguire le cose in ordine 
@@ -18,21 +15,22 @@ altrimenti le esegue in ordine di rapidità di
 esecuzione 
 */
 
-//trasforma res in un json
-.then(res => res.json())
 
 //se si va in /richiesta fa la richiesta, mandando un json 
-.then(data => {
-	app.get("/richiesta", (req, res) => {
-  res.status(200).json(data);
-	})
+
+app.get("/richiesta", (req, res) => {
+
+	fetch("https://api.paninidellospalla.it/info", {
+	method: "GET"
 })
+	//trasforma res in un json
+	.then(res => res.json())
+	
+	.then(data => {
+		res.status(200).json(data);
+		}
 
-
-//lo esegue se la promise dà un errore (ERR lo inizializzo ora)
-.catch((ERR) => {
-	console.log(ERR)
-});
+})
 
 //mette in ascolto alla porta 3000
 app.listen(3000, () => {
