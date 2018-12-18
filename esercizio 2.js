f89b812bf850814fb6ce50f834c5a215991728db
Francesco Musi <francesco.musi03@gmail.com>
2018-12-18 16:23:28 +0100

    work

--- esercizio 2.js
+++ esercizio 2.js
@@ -1,4 +1,4 @@
-//importo i framework (libreria)
+//importo i framework (librerie)
 const fetch = require("node-fetch");
 const express = require("express");
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 //2° argomento = metodo
 //3° argomento = body (non serve in questo caso)
 
-fetch("https//api.paninidellospalla.it/info", {
+fetch("https://api.paninidellospalla.it/info", {
 	method: "GET"
 })
 
@@ -21,23 +21,21 @@ esecuzione
 //trasforma res in un json
 .then(res => res.json())
 
-//stampa sul terminale res
-.then(res => {
-	console.log(res)
+//se si va in /richiesta fa la richiesta, mandando un json 
+.then(data => {
+	app.get("/richiesta", (req, res) => {
+  res.status(200).json(data);
+	})
 })
+
 
 //lo esegue se la promise dà un errore (ERR lo inizializzo ora)
 .catch((ERR) => {
 	console.log(ERR)
 });
 
-//se si va in /richiesta fa la richiesta
-app.get("/richiesta", (req, res) => {
-  res.status(200).send(res);
-});
-
-
 //mette in ascolto alla porta 3000
 app.listen(3000, () => {
-	console.log("App listening on port 3000...")
+	console.log("App in ascolto sulla porta 3000...")
 })
+
